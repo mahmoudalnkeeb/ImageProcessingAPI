2ee377d25689ec2599f79bfe6ce6c2ccc1adc097
Mahmoud Alnkeeb <59762294+mahmoudalnkeeb@users.noreply.github.com>
2022-01-23 00:43:22 +0200

    remove build folder

--- build/middlewares/logger.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.logger = void 0;
-const logger = (req, res, next) => {
-    const url = req.url;
-    console.log(`info: ${url} is visited`);
-    next();
-};
-exports.logger = logger;
