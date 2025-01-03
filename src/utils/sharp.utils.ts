2c57aa50fde7ae7999fea8a712dc587f5506cc83
mahmoudalnkeeb <ramadanalnkeeb2018@gmail.com>
2025-01-04 01:40:09 +0200

    feat(imageProcessing):added crop , change format and watermark logic

--- src/utils/sharp.utils.ts
+++ src/utils/sharp.utils.ts
@@ -1,9 +1,38 @@
-import sharp from 'sharp';
+import sharp, { FormatEnum } from 'sharp';
 
 export function resizeImage(image: Buffer, width: number, height: number) {
   return sharp(image).resize(width, height);
 }
 
-export function crop(image: Buffer, width: number, height: number, gravity: number) {
-  return sharp(image).resize(width, height);
+export function crop(image: Buffer, width: number, height: number, top: number, left: number) {
+  return sharp(image).extract({
+    height,
+    width,
+    top,
+    left,
+  });
+}
+
+export function format(image: Buffer, format: keyof FormatEnum, quality: number) {
+  return sharp(image).toFormat(format, { quality });
+}
+
+export function watermark(
+  image: Buffer,
+  watermarkText: string,
+  fontFamily: string,
+  fontSize: string,
+  fill: string,
+  top: number,
+  left: number
+) {
+  return sharp(image).composite([
+    {
+      input: Buffer.from(
+        `<svg><text x="${left}" y="${top}" font-family="${fontFamily}" font-size="${fontSize}" fill="${fill}">${watermarkText}</text></svg>`
+      ),
+      top,
+      left,
+    },
+  ]);
 }
